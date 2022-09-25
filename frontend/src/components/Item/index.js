6bd84d1589450745be65cbdc4bc046c260d4a1cf
Tomer Brosh <bomer.apps@gmail.com>
2022-09-26 01:57:08 +0300

    Revert "Fixed broken image showing in the UI when the item has no image"
    This reverts commit 2742bc869c1caf087ae4152e7ebf1127344adf1a.
    Reverting the frontend fix in order to commit a different fix, which will show a placeholder image if there is no image for the item.

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -41,8 +41,6 @@ class Item extends React.Component {
     const markup = {
       __html: marked(this.props.item.description, { sanitize: true }),
     };
-    const isImageExists =
-      this.props.item.image != null && this.props.item.image.length >= 1;
     const canModify =
       this.props.currentUser &&
       this.props.currentUser.username === this.props.item.seller.username;
@@ -50,22 +48,16 @@ class Item extends React.Component {
       <div className="container page">
         <div className="text-dark">
           <div className="row bg-white p-4">
-            {isImageExists && (
-              <div className="col-6">
-                <img
-                  src={this.props.item.image}
-                  alt={this.props.item.title}
-                  className="item-img"
-                  style={{
-                    height: "500px",
-                    width: "100%",
-                    borderRadius: "6px",
-                  }}
-                />
-              </div>
-            )}
+            <div className="col-6">
+              <img
+                src={this.props.item.image}
+                alt={this.props.item.title}
+                className="item-img"
+                style={{ height: "500px", width: "100%", borderRadius: "6px" }}
+              />
+            </div>
 
-            <div className={isImageExists ? "col-6" : "col-12"}>
+            <div className="col-6">
               <h1>{this.props.item.title}</h1>
               <ItemMeta item={this.props.item} canModify={canModify} />
               <div dangerouslySetInnerHTML={markup}></div>

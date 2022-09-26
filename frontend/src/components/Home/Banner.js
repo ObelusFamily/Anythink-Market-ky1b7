fb461bdf824555647260ee758716c377d713b81e
Tomer Brosh <bomer.apps@gmail.com>
2022-09-27 00:58:53 +0300

    The search box is now only displayed if you click the word 'get'

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import SearchBox from "./SearchBox";
 
 const Banner = (props) => {
+  const [isSearchActive, setIsSearchActive] = useState(false);
+
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <SearchBox
-            onTitleSearch={props.onTitleSearch}
-            onResetSearch={props.onResetSearch}
-          />
-          <span>the cool stuff.</span>
+          <span id="get-part">A place to </span>
+          <span
+            onClick={() => {
+              setIsSearchActive(true);
+            }}
+          >
+            get
+          </span>
+          {isSearchActive && (
+            <SearchBox
+              onTitleSearch={props.onTitleSearch}
+              onResetSearch={props.onResetSearch}
+            />
+          )}
+          <span> the cool stuff.</span>
         </div>
       </div>
     </div>
